9c7a309d02e6eb48ca19eba731219cfc56da88ec
Omar Crosby <omar.crosby@gmail.com>
2023-02-05 12:58:57 -0500

    feat(server): removing the defunct player.test.ts

--- server/__tests__/common/player.test.ts
+++ server/__tests__/common/player.test.ts
@@ -1,46 +1,41 @@
-import {afterEach, beforeEach, describe, expect, test} from '@jest/globals';
+import {beforeEach, describe, expect, test} from '@jest/globals';
 
-import {Player, PlayerBuilder} from '../../src/common/Player';
+import {Player} from '../../src/common/Player';
 
-describe('player module', () => {
-    describe('Player', () => {
-        let player: Player;
+describe('Player', () => {
+    let player: Player;
 
-        beforeEach(() => {
-            player = new Player();
-        });
-
-        test('default constructor', () => {
-            expect(player.getName()).toBe(null);
-            expect(player.getJerseyNumber()).toBe(null);
-            expect(player.getPosition()).toBe(null);
-        });
-
-        test('setName updates name', () => {
-            player.setName('John Doe');
-
-            expect(player.getName()).toBe('John Doe');
-        });
-
-        test('setJerseyNumber updates jersey number', () => {
-            player.setJerseyNumber(42);
-            expect(player.getJerseyNumber()).toBe(42);
-        });
-
-        test('setPosition updates position', () => {
-            player.setPosition('QB');
-            expect(player.getPosition()).toBe('QB');
-        });
-
-        test('toString returns name, jersey number, and position', () => {
-            player.setName('John Doe');
-            player.setJerseyNumber(42);
-            player.setPosition('QB');
-
-            expect(player.toString()).toBe('John Doe 42 QB');
-        });
+    beforeEach(() => {
+        player = new Player();
     });
 
-    describe('PlayerBuilder', () => {
+    test('default constructor', () => {
+        expect(player.getName()).toBe(null);
+        expect(player.getJerseyNumber()).toBe(null);
+        expect(player.getPosition()).toBe(null);
+    });
+
+    test('setName updates name', () => {
+        player.setName('John Doe');
+
+        expect(player.getName()).toBe('John Doe');
+    });
+
+    test('setJerseyNumber updates jersey number', () => {
+        player.setJerseyNumber(42);
+        expect(player.getJerseyNumber()).toBe(42);
+    });
+
+    test('setPosition updates position', () => {
+        player.setPosition('QB');
+        expect(player.getPosition()).toBe('QB');
+    });
+
+    test('toString returns name, jersey number, and position', () => {
+        player.setName('John Doe');
+        player.setJerseyNumber(42);
+        player.setPosition('QB');
+
+        expect(player.toString()).toBe('John Doe 42 QB');
     });
 });
