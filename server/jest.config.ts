696ffbca24e60687a032432b8f330c1a67fea0c9
Omar Crosby <omar.crosby@gmail.com>
2023-02-05 15:11:38 -0500

    feat(server): updating coverage configuration

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -8,6 +8,19 @@ import { defaults as tsjPreset } from 'ts-jest/presets'
 // import { jsWithBabelESM as tsjPreset } from 'ts-jest/presets';
 
 const jestConfig: JestConfigWithTsJest = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  coverageDirectory: 'coverage',
+  verbose: true,
+  testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 13,
+      lines: 13,
+      statements: 13
+    }
+  },
   // [...]
   transform: {
     ...tsjPreset.transform,
