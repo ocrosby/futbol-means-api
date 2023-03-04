5faa170db174dd7e187278634ae9092330306a07
Omar Crosby <omar.crosby@gmail.com>
2023-03-04 07:51:16 -0500

    chore: wip

--- server/tests/controllers/health.ts
+++ server/tests/controllers/health.ts
@@ -1,6 +1,14 @@
 import request from 'supertest'
 import { Express } from 'express-serve-static-core'
 
+import { createServer } from "../../src/app";
+
+let server: Express
+
+beforeAll(async () => {
+  server = await createServer().app
+})
+
 describe('GET /health-check', () => {
   it('should return 200', () => {
 
