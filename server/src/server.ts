fccdc5ce401f493857f2910604c78e79ab984382
Omar Crosby <omar.crosby@gmail.com>
2023-03-04 06:52:05 -0500

    chore: refactoring for testability

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,10 @@
 import * as dotenv from 'dotenv'
 
-import App from './app'
-
+import { createApp } from './app'
 import { validateEnv } from './utils/validateEnv'
 
 dotenv.config()
 
 validateEnv()
 
-let port: number = Number(process.env.API_LOCAL_PORT)
-
-if (Number.isNaN(port)) port = 8000
-
-const app = new App(port)
-
-app.listen()
+createApp(process.env.API_LOCAL_PORT).listen()
