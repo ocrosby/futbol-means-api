2a5291dfdb1280d90ed74cb0db3cbe40c4a242bf
Omar Crosby <omar.crosby@gmail.com>
2023-03-06 05:13:04 -0500

    feat(health): adding a health check endpoint

--- server/src/controllers/health.controller.ts
+++ server/src/controllers/health.controller.ts
@@ -1,9 +1,39 @@
-import { Controller, Get, Route } from 'tsoa'
+import { Controller, Get, Route, Tags } from 'tsoa'
 
-@Route('api/health-check')
-export class HealthCheckController extends Controller {
+import mongoose from 'mongoose'
+
+@Route('health-check')
+@Tags('Health')
+export class HealthController extends Controller {
   @Get('/')
   public async checkHealth() {
-    return { msg: 'Hello'}
+    let state: string;
+
+    switch(mongoose.connection.readyState) {
+      case 0:
+        state = 'disconnected'
+        break
+      case 1:
+        state = 'connected'
+        break
+      case 2:
+        state = 'connecting'
+        break
+      case 3:
+        state = 'disconnecting'
+        break
+      default:
+        state = 'unknown'
+    }
+
+
+    if (mongoose.connection.readyState === 1) {
+      // connected
+      this.setStatus(200);
+    } else {
+      this.setStatus(500);
+    }
+
+    return { msg: state }
   }
 }
