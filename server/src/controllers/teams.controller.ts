48354953cd91cd5451839fc0a670f052b231883c
Omar Crosby <omar.crosby@gmail.com>
2023-03-06 05:03:51 -0500

    fix(swagger): fixing the base path issue

--- server/src/controllers/teams.controller.ts
+++ server/src/controllers/teams.controller.ts
@@ -3,7 +3,10 @@ import {
   Controller,
   Get,
   Path,
+  Put,
   Post,
+  Patch,
+  Delete,
   Route,
   Tags,
   SuccessResponse
@@ -11,6 +14,7 @@ import {
 
 import { ITeam, ITeamDocument } from '../models/team.model'
 import TeamsService from '../services/teams.service'
+import { IPatchOperation } from '../interfaces/patch.interface';
 
 @Route('teams')
 @Tags('Team')
@@ -34,5 +38,32 @@ export class TeamsController extends Controller {
   ): Promise<ITeamDocument> {
     this.setStatus(201) // set return status 201
     return await TeamsService.addOne(requestBody)
+  }
+
+  @Put('{id}')
+  public async updateTeam (
+    @Path() id: string,
+    @Body() requestBody: ITeam
+  ): Promise<ITeamDocument | null> {
+    return await TeamsService.updateOne(id, requestBody)
+  }
+
+  @Delete('{id}')
+  public async deleteTeam (
+    @Path() id: string
+  ): Promise<void> {
+    await TeamsService.delete(id)
+
+    return Promise.resolve()
+  }
+
+  @Patch('{id}')
+  public async patchTeam(
+    @Path() id: string,
+    @Body() requestBody: IPatchOperation[]
+  ): Promise<void> {
+    await TeamsService.patch(id, requestBody)
+
+    return Promise.resolve()
   }
 }
