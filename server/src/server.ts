55320d2ec8f5f6ef76d0f536045bb97d0a05fb4e
Omar Crosby <omar.crosby@gmail.com>
2023-03-09 06:44:53 -0500

    feat(server): refactoring

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import * as dotenv from 'dotenv'
 import { validateEnv } from "./utils/validateEnv";
 
+import Logger from './utils/logger';
+
 dotenv.config();
 validateEnv();
 
@@ -8,4 +10,6 @@ import app from './app'
 
 const PORT: Number = Number(process.env.API_LOCAL_PORT);
 
-app.listen(PORT, () => console.log(`running on port ${PORT}`));
+app.listen(PORT, () => {
+  Logger.info(`listening on port ${PORT}`)}
+)
