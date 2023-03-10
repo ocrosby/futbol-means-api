b49b3e9aed9b0d1810953c8c0fe3e9a699dd52a1
Omar Crosby <omar.crosby@gmail.com>
2023-03-10 03:48:36 -0500

    chore: adding a comment to ensure I don't forget to look into this import/require thing

--- server/tests/integration/app.spec.ts
+++ server/tests/integration/app.spec.ts
@@ -1,5 +1,10 @@
 import app from '../../src/app'
 
+// Note: I need to look into this in more detail as I think this is responsible for the strange
+//       "error: Unable to parse undefined:undefined with URL" output message I'm seeing in the tests.
+//       It seems to work, but I don't like that error message.
+//
+// https://www.typescriptlang.org/docs/handbook/modules.html#export--and-import--require
 import request = require('supertest')
 
 describe('Test API', () => {
@@ -7,25 +12,25 @@ describe('Test API', () => {
     let res: request.Response | null;
 
     beforeEach(async () => {
-      res = await request(app).get('/healthcheck')
+        res = await request(app).get('/healthcheck')
     })
 
     afterEach(() => {
       res = null;
     })
 
-    test('should return a 500', async () => {
+    test('GET /healthcheck should have a 500 statusCode', async () => {
       expect(res?.statusCode).toEqual(500)
     })
 
-    test('should return the expected body', async () => {
+    test('GET /healthcheck should have a disconnected mongooseReadyState', async () => {
       expect(res?.body.state).toEqual("unhealthy")
       expect(res?.body.mongooseReadyState).toEqual('disconnected')
     })
   })
 
   describe('Teams', () => {
-    test.skip('it should return a list of teams', async () => {
+    test.skip('GET /api/teams should return a list of teams', async () => {
       const res = await request(app).get('/api/teams')
       expect(res.statusCode).toEqual(200)
       expect(res.body).toEqual({msg: "Hello World!"})
